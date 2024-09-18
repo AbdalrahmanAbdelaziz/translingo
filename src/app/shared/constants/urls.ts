4ed9dc6ac59b0cf128e36690e074cce69a9261b6
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-09-18 14:07:53 +0300

    changes

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -6,8 +6,9 @@
 // export const RANDOM_WORDS_URL = (page: number, pageSize: number) => `${WORDS_URL}?PageNumber=${page}&PageSize=${pageSize}`;
 
 // Use a relative path for API endpoints
-const BASE_URL = 'http://studywithmina.runasp.net/api';
+const BASE_URL = '/api';
 
 export const WORDS_URL = BASE_URL + '/Words';
 export const TRANSLATE_WORD_URL = (word: string) => `${WORDS_URL}/${word}`;
 export const RANDOM_WORDS_URL = (page: number, pageSize: number) => `${WORDS_URL}?PageNumber=${page}&PageSize=${pageSize}`;
+
