30fb9af8f1a6a7860b480f86f944a582a2258496
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-09-19 11:35:41 +0300

    changes

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -1,12 +1,6 @@
-// const BASE_URL = 'http://studywithmina.runasp.net';
+const BASE_URL = "http://studywithmina.runasp.net";
 
-
-// export const WORDS_URL = BASE_URL + '/api/Words';
-// export const TRANSLATE_WORD_URL = (word: string) => `${WORDS_URL}/${word}`;
-// export const RANDOM_WORDS_URL = (page: number, pageSize: number) => `${WORDS_URL}?PageNumber=${page}&PageSize=${pageSize}`;
-
-
-export const WORDS_URL = '/api/Words';  // Removed BASE_URL for relative path
-export const TRANSLATE_WORD_URL = (word: string) => `${WORDS_URL}/${word}`;
-export const RANDOM_WORDS_URL = (page: number, pageSize: number) => `${WORDS_URL}?PageNumber=${page}&PageSize=${pageSize}`;
+export const WORDS_URL = BASE_URL + '/api/Words';  // Removed BASE_URL for relative path
+export const TRANSLATE_WORD_URL = (word: string) =>  BASE_URL + `${WORDS_URL}/${word}`;
+export const RANDOM_WORDS_URL = (page: number, pageSize: number) =>  BASE_URL +  `${WORDS_URL}?PageNumber=${page}&PageSize=${pageSize}`;
 
