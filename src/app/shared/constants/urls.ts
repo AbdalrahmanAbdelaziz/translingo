1e08d56555bc587fd9e5b0608df3479ee35f8a78
Abdalrahman Abdelaziz <abdalrahmanabdelaziz03@gmail.com>
2024-09-19 14:42:37 +0300

    changes

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -6,7 +6,7 @@
 
 
 
-const BASE_URL = "http://studywithmina.runasp.net";
+const BASE_URL = "https://studywithmina.runasp.net";
 
 export const WORDS_URL = BASE_URL + '/api/Words';  // Removed BASE_URL for relative path
 export const TRANSLATE_WORD_URL = (word: string) =>  `${WORDS_URL}/${word}`;
